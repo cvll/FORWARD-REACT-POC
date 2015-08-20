e764a8989b72586db80fc1b29330c428e2c93b06
cvll <chrislopez1228@gmail.com>
2015-08-19 21:22:28 -0700

    basic functionality

--- src/js/index-nonjsx.js
+++ src/js/index-nonjsx.js
@@ -1,28 +1,39 @@
-var mountNode = document.getElementById('map');
-var map;
-//var data;
-var ref;
-var filterRef;
-var item_data;
-var ORIGINAL_DATA;
-var fNode = document.getElementById('filter');
+// DOM references
+var mapNode = document.getElementById('map');
+var filterNode = document.getElementById('filter');
 
+// Reference to React objects
+var reactMapReference;
+var reactFilterReference;
+
+// Data
+var CATEGORIES;
+var ORIGINAL_DATA;
+
+
+// Notify function provided to the React Google Map to be run upon being notified by React Filter
 var notifyFunc = function(cat) {
   this.setState({filter:cat});
 };
 
 
+// Simulates live data by manipulating the marker locations
 var updateLocations = function() {
-  var curr_data = ref.getLocations();
+  var curr_data = reactMapReference.getLocations();
       for (var i = 0 ; i < curr_data.length ; i++) { 
         var lat = curr_data[i].loc.lat() + (Math.random()/1000);
         var lng = curr_data[i].loc.lng() + (Math.random()/1000);
         var newLoc = new google.maps.LatLng(lat.toFixed(6),lng.toFixed(6));
         newLoc.category = curr_data[i].category;
-        ref.updateMarkerLocation(i,newLoc);
+        reactMapReference.updateMarkerLocation(i,newLoc);
       } 
 }
 
+
+
+
+
+// React Filter Component
 var MyFilterComponent = React.createClass({displayName:"MyFilterComponent",
     getInitialState: function() {
       return {filter:'All'};
@@ -30,10 +41,9 @@ var MyFilterComponent = React.createClass({displayName:"MyFilterComponent",
     onChange: function() {
       var dom = this.getDOMNode();
       var newCategory = dom.options[dom.options.selectedIndex].value;
-      
-      this.setState({filter: newCategory});
-      
-      this.props.map.notify(ref,newCategory);
+    
+      this.setState({filter: newCategory});  
+      this.props.map.notify(reactMapReference,newCategory);
     },
     render: function() {
       var createOption = function(item,idx) {
@@ -43,11 +53,11 @@ var MyFilterComponent = React.createClass({displayName:"MyFilterComponent",
     }
 });
 
-
+// React Google Component
 var MyGoogleMap = React.createClass({displayName: "MyGoogleMap",
   // starting state
   getInitialState: function() {
-    return {filter:'All',initial:true,markers: []};
+    return {map:null,filter:'All',initial:true,markers: []};
   },
   getMarkers() {
     return this.state.markers;
@@ -62,7 +72,7 @@ var MyGoogleMap = React.createClass({displayName: "MyGoogleMap",
     
     return locs;
   },
-  // used to update marker locations
+  // used to update a marker's location
   updateMarkerLocation: function(i,loc) {
     var m = this.getMarkers();
     m[i].setPosition(loc);
@@ -88,15 +98,15 @@ var MyGoogleMap = React.createClass({displayName: "MyGoogleMap",
   showAllMarkers: function() {
     var m = this.getMarkers();
     for (var i = 0;  i < m.length ; i++) {
-      m[i].setMap(map);
+      m[i].setMap(this.state.map);
     }
   },
   render: function() {
-    if (!map) {
+    if (!this.state.map) {
       this.state.initial = false;
       
       // create the map
-      map = new google.maps.Map(document.getElementById('map'), {
+      this.state.map = new google.maps.Map(mapNode, {
         center: {lat: 32.879632, lng: -117.235687},
         zoom: 15,
         offsetWidth: 0
@@ -105,15 +115,16 @@ var MyGoogleMap = React.createClass({displayName: "MyGoogleMap",
     
 
       var createMarkers = function(o,idx) {
-        var loc = o.loc;
-        var map_holder = o.category == this.state.filter || this.state.filter == 'All' ? map : null;
-        var marker = new google.maps.Marker({
-              position: loc,
-              map: map_holder,
-              category: o.category
-        });
-        this.state.markers[idx] = marker;
-        return React.createElement('noscript',{key:idx,category:o.category},idx);
+          var loc = o.loc;
+          var map_holder = o.category == this.state.filter || this.state.filter == 'All' ? this.state.map : null;
+          var marker = new google.maps.Marker({
+                position: loc,
+                map: map_holder,
+                category: o.category
+          });
+        
+          this.state.markers[idx] = marker;
+          return React.createElement('noscript',{key:idx,category:o.category},idx);
       }
       return React.createElement('div',null,this.props.locations.map(createMarkers,this));
     } else {
@@ -125,7 +136,7 @@ var MyGoogleMap = React.createClass({displayName: "MyGoogleMap",
       } else {
         for (var i = 0 ; i < m.length ; i++) {
           if (m[i].category == this.state.filter) {
-            m[i].setMap(map);
+            m[i].setMap(this.state.map);
           } else {
             m[i].setMap(null);
           }
@@ -138,7 +149,6 @@ var MyGoogleMap = React.createClass({displayName: "MyGoogleMap",
 
 
 var init = function() {
-  
   ORIGINAL_DATA = [
    {loc: new google.maps.LatLng (32.879632,-117.235687), category: 'Car'},
    {loc: new google.maps.LatLng (32.882206, -117.229898), category: 'Car'},
@@ -146,16 +156,11 @@ var init = function() {
    {loc: new google.maps.LatLng (32.881755, -117.235091), category: 'Truck'},
    {loc: new google.maps.LatLng (32.876998, -117.235048), category: 'Van'}
 ];
-  //data = ORIGINAL_DATA;
-  
-  
-  item_data = ['All','Car','Truck'];
+  CATEGORIES = ['All','Car','Truck'];
  
-  ref = React.render(React.createElement(MyGoogleMap, {locations:ORIGINAL_DATA,notify:notifyFunc}), mountNode);
+  reactMapReference = React.render(React.createElement(MyGoogleMap, {locations:ORIGINAL_DATA,notify:notifyFunc}), mapNode);
   
-  filterRef = React.render(React.createElement(MyFilterComponent,{items:item_data,map:ref}),document.getElementById('filter'));
-  
-  
+  reactFilterReference = React.render(React.createElement(MyFilterComponent,{items:CATEGORIES,map:reactMapReference}),filterNode);
   
   
   
@@ -164,10 +169,10 @@ var init = function() {
   
   
   
+  
+  // Used to simulate live data by constantly updating the marker locations
   setInterval(function() {
-    //console.log("interval running..");
       updateLocations();
-    //console.log("interval done!");
      }, 150);
   
 }
